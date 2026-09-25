2d1cc075149d1c0c51ceca362474e2d3ef82405d
bayshorerelicsllc <bayshorerelicsllc@gmail.com>
2026-09-25 14:19:32 -0500

    User wording: See our Privacy Policy and Terms of Service

--- assets/js/login-modal.js
+++ assets/js/login-modal.js
@@ -193,9 +193,9 @@ var TURNSTILE_SITE_KEY = '0x4AAAAAAFDXwhByfdKoDTPH';
   var EYE_OFF_SVG = '<svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"/><path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19"/><line x1="1" y1="1" x2="23" y2="23"/></svg>';
 
   var CAPTCHA_NOTE = '<p class="bsr-captcha-note">This site is protected by ' +
-    '<span class="bsr-cf-word">Cloudflare</span><br>Our ' +
+    '<span class="bsr-cf-word">Cloudflare</span>.<br>See our ' +
     '<a href="/privacy/">Privacy Policy</a> and ' +
-    '<a href="/terms/">Terms of Service</a> apply.</p>';
+    '<a href="/terms/">Terms of Service</a>.</p>';
 
   function circlesHtml() {
     return '' +
