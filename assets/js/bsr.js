8d0118036c67192152f3722cf6a71db4aa705551
bayshorerelicsllc <bayshorerelicsllc@gmail.com>
2026-09-25 08:34:48 -0500

    Bump login-modal.js version for new login flow

--- assets/js/bsr.js
+++ assets/js/bsr.js
@@ -412,7 +412,7 @@
     if (loginModalLoading) return loginModalLoading;
     loginModalLoading = new Promise(function (resolve) {
       var s = document.createElement('script');
-      s.src = '/assets/js/login-modal.js?v=20260925f';
+      s.src = '/assets/js/login-modal.js?v=20260925g';
       s.onload = function () { resolve(); };
       s.onerror = function () { resolve(); };
       document.head.appendChild(s);
