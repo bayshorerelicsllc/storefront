d173b48118cf19eb8a6aaecbff4a2cf0d1e160ec
bayshorerelicsllc <bayshorerelicsllc@gmail.com>
2026-09-25 13:55:47 -0500

    Login blurb: keep Cloudflare icon and word on same line

--- assets/js/login-modal.js
+++ assets/js/login-modal.js
@@ -77,6 +77,7 @@ var TURNSTILE_SITE_KEY = '0x4AAAAAAFDXwhByfdKoDTPH';
     '.bsr-captcha-note{text-align:center;font-size:.78rem;color:#8a8172;margin:28px 0 0;line-height:1.6}',
     '.bsr-captcha-note a{color:#2a241b}',
     '.bsr-cf-mark{height:1.3em;width:auto;vertical-align:-0.25em;margin-right:.45em}',
+    '.bsr-cf-word{white-space:nowrap}',
     '.bsr-loginbtn{width:100%;min-height:58px;background:#1e4d33;color:#fff;border:none;border-radius:4px;',
     'font-size:.98rem;font-weight:700;letter-spacing:.07em;cursor:pointer;font-family:inherit;margin-top:6px;',
     'transition:background .15s}',
@@ -193,7 +194,7 @@ var TURNSTILE_SITE_KEY = '0x4AAAAAAFDXwhByfdKoDTPH';
   var EYE_OFF_SVG = '<svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94"/><path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19"/><line x1="1" y1="1" x2="23" y2="23"/></svg>';
 
   var CAPTCHA_NOTE = '<p class="bsr-captcha-note">This site is protected by ' +
-    '<img class="bsr-cf-mark" src="/assets/img/cloudflare-mark.png" alt="">Cloudflare and our ' +
+    '<span class="bsr-cf-word"><img class="bsr-cf-mark" src="/assets/img/cloudflare-mark.png" alt="">Cloudflare</span> and our ' +
     '<a href="/privacy/">Privacy Policy</a> and ' +
     '<a href="/terms/">Terms of Service</a> apply.</p>';
 
